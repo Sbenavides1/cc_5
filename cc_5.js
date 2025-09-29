d2f6c331b87297b0e2cb49c9be6c5ae7c82891ae
Sofia Benavides <Sbenavidesarias@usf.edu>
2025-09-28 21:48:39 -0400

    Array of employees

--- cc_5.js
+++ cc_5.js
@@ -1,2 +1,13 @@
 // Coding Challenge 5
 
+// Array of employee objects
+let employees = [
+    { name: "Alice", hourlyRate: 20, hoursWorked: 40 },
+    { name: "Bob", hourlyRate: 15, hoursWorked: 35 },
+    { name: "Juan", hourlyRate: 25, hoursWorked: 30 },
+    { name: "Diana", hourlyRate: 30, hoursWorked: 45 },
+    { name: "Sofia", hourlyRate: 18, hoursWorked: 50 }
+];
+
+// Create an array of 3–5 employee objects. Each should include: name, hourlyRate, hoursWorked.
+
